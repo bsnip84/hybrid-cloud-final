ad36bfd25cb4e8eefabd5f3deaf512db8b1c22b9
belal <alnatourbelal120@gmail.com>
2026-08-17 11:48:18 +0200

    Add VNet, AKS, and Key Vault modules

---
+++ infra/terraform/environments/dev/aks.tf
@@ -0,0 +1,17 @@
+﻿resource "azurerm_kubernetes_cluster" "main" {
+  name                = "aks-hybrid-dev"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "akshybrid"
+
+  default_node_pool {
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_B2s"
+    vnet_subnet_id = azurerm_subnet.aks.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
